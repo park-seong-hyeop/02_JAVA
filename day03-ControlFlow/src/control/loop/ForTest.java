817873c4ed5b9936b2927b069c475562d3ac6194
PARK SEONG HYEOP <nasu327@gmail.com>
2019-07-10 14:44:12 +0900

    for 추가문제

---
+++ day03-ControlFlow/src/control/loop/ForTest.java
@@ -0,0 +1,33 @@
+package control.loop;
+/** 
+ * 1. 50 ~ 0 까지 짝수만 역순으로 출력
+ *   (1) continue 없이
+ *   (2) comtinue 사용
+ *   
+ * ------------------------------------  
+ * 2. 1 ~ 10 까지 제곱표를 출력
+ *    1 x 1 = 1
+ *    2 x 2 = 4
+ *    
+ *    ...
+ *    10 x 10 =100
+ * ------------------------------------   
+ *    
+ *    
+ * @author Administrator
+ *
+ */
+public class ForTest {
+
+	public static void main(String[] args) {
+		// 문제1)
+		// continue 없이
+		for (int idx = 50; idx > 0; idx--) {
+			System.out.printf("idx=%2d%n", idx);
+		}
+		// comtinue 사용
+		for ()
+
+		}
+
+	}
